1957c1adbc10fcadd8ae346aeb6eca6e9726a461
Aayush Siwa U <22052177@kiit.ac.in>
2025-01-30 10:48:09 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+function App() {
+    return (
+        <>
+            <div className="bg-amber-100 dark:bg-black h-screen">
+              <Navbar/>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-gray-800 h-16">
+        {/* brand-name */}
+        <div className="text-3xl text-zinc-50">
+          IdeaBoard
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
